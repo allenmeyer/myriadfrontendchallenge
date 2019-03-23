317200272f884394fc293d2de975229e70e0a7be
allenmeyer <47233062+allenmeyer@users.noreply.github.com>
2019-03-23 18:16:46 -0500

    Reformatted PokeList, added PokeDetails, nav buttons
    - Changed PokeDetails class to PokeList
        -> Added extra minor details to pokemon on list view as seen in UI spec
    - Added back and forward buttons to PokePage to navigate through the pages of pokemon
        -> TODO: possibly remove next page button if there are no more pokemon to be shown.
    - Added new PokeDetails class which is the individual view for a pokemon
        -> Displays rudimentary details for each pokemon
        -> Implemented 'Go Back' button on page which goes functions the same as the back arrow

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -11,9 +11,16 @@ function PokeForm (props){
 	);
 }
 
-
+function arrayToString(array) {
+	var arrayData = "";
+	for (var i = 0; i < array.length; i++) {
+		arrayData += array[i] + ' '
+	}
+	return arrayData;
+}
 
 class PokeDetails extends React.Component {
+	
 	constructor (props) {
 		super(props);
 		this.state = {
@@ -21,8 +28,58 @@ class PokeDetails extends React.Component {
 		}
 	}
 	
+	printStats(stats) {
+		return (
+			<div>
+				<p> Attack: {stats['attack']}, Defense: {stats['defense']} </p>
+				<p> SpAttack: {stats['special-attack']}, SpDefense: {stats['special-defense']} </p> 
+				<p> Health: {stats['hp']}, Speed: {stats['speed']} </p>
+			</div>
+		);
+	}
+	
+	componentDidMount() {
+		const id = this.props.match.params.id;
+		const url = `https://intern-pokedex.myriadapps.com/api/v1/pokemon/${id}`;
+		axios.get(url).then( (res) => {
+			let data = [];
+			data.push(res.data.data);
+			this.setState({data});
+		});
+	}
+	
+	render() {
+		let pokedata = this.state.data.map((pokemon) => {
+			return (
+				<div>
+					<button onClick={this.props.history.goBack}> Go Back </button> 
+					<p> {pokemon.name}, the {pokemon.genus} </p>
+					<img src = {pokemon.image} alt = 'Pokemon' />
+					<p> {arrayToString(pokemon.types)} </p>
+					<p> Abilities: {arrayToString(pokemon.abilities)} </p>
+					{pokemon.description}
+					{this.printStats(pokemon.stats)}
+				</div>
+			);
+		});
+		return <div>{pokedata[0]}</div>;
+	}
+}
+
+class PokeList extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
+			data: [],
+		}
+	}
+	
+	viewPokemonDetails = (id) => {
+		window.location.href = `/viewPokemon/${id}`;
+	}
+	
 	componentDidMount() { // Perform get request for page of pokemon data
-		const poke = this.props.searchPokeName;
+		const poke = this.props.search_name;
 		const page = this.props.page;
 		const url = poke ? 
 						page ? `https://intern-pokedex.myriadapps.com/api/v1/pokemon?name=${poke}&page=${page}` :
@@ -41,9 +98,10 @@ class PokeDetails extends React.Component {
 	render() {
 		let pokedata = this.state.data.map( (pokemon) => {
 			return (
-				<td> 
-					<img src = {pokemon.image} alt = 'pokemon' /> 
+				<td onClick={() => this.viewPokemonDetails(pokemon.id)}> 
+					<img src = {pokemon.image} alt = 'Pokemon' /> 
 					<p> {pokemon.name} </p>
+					<p> {arrayToString(pokemon.types)} </p>
 				</td>
 			);
 		});
@@ -85,12 +143,56 @@ class PokeDetails extends React.Component {
 
 
 class PokePage extends React.Component {
+	
+	searchPokes = (e) => {
+		e.preventDefault();
+		const poke = e.target.elements.pokename.value;
+		window.location.href = `/name/${poke}/`;
+	}
+	
+	navButtons() {
+		if (this.props.match.params.page > 1 ) {
+			return (
+				<div>
+					<button onClick={() => this.viewPreviousPage()}> Previous Page </button>
+					<button onClick={() => this.viewNextPage()}> Next Page </button>						
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<button onClick={() => this.viewNextPage()}> Next Page </button>
+				</div>
+			);
+		}
+	}
+	
+	viewNextPage = () => {
+		const poke = this.props.match.params.poke;
+		const page = parseInt(this.props.match.params.page,10) + 1;
+		const url = poke ? 
+						page ? `/name/${poke}/page/${page}/` :
+							   `/name/${poke}/page/${2}/` :
+						page ? `/page/${page}/` :
+							   `/page/${2}/`;
+		window.location.href = url;
+	}
+	
+	viewPreviousPage = () => {
+		const poke = this.props.match.params.poke;
+		const page = this.props.match.params.page - 1;
+		const url = poke ? `/name/${poke}/page/${page}/` :
+						   `/page/${page}/`;
+		window.location.href = url;
+	}
+	
 	render() {
 		return (
 			<div>
-				<PokeForm onSubmit={this.props.searchPokes}/>
-				<PokeDetails searchPokeName={this.props.match.params.poke}
-									   page={this.props.match.params.page}
+				<PokeForm onSubmit={this.searchPokes}/>
+				{this.navButtons()}
+				<PokeList search_name={this.props.match.params.poke}
+								 page={this.props.match.params.page}
 				/>
 			</div>
 		);
@@ -98,34 +200,27 @@ class PokePage extends React.Component {
 }
 
 class App extends Component {
-	
-	// Not currently implemented
-	// Need to use to direct to /name/${poke}
-	searchPokes = (e) => {
-		e.preventDefault();
-		const poke = e.target.elements.pokename.value;
-		axios.get(`https://intern-pokedex.myriadapps.com/api/v1/pokemon?name=${poke}`)
-			.then((res) => {
-				const pokes = res.data.data;
-				console.log(pokes);
-			})
-	}
-
 	render() {
 		return (
 			<BrowserRouter>
 				<Switch>
 					<Route path='/name/:poke/page/:page' component={(props)=>
-						<PokePage {...props} searchPokes={this.searchPokes}  />
+						<PokePage {...props}  />
+					} />
+					<Route path='/page/:page/name/:poke' component={(props)=>
+						<PokePage {...props}  />
 					} />
 					<Route path='/name/:poke' component={(props)=>
-						<PokePage {...props} searchPokes={this.searchPokes}  />
+						<PokePage {...props}  />
 					} />
 					<Route path='/page/:page' component={(props)=>
-						<PokePage {...props} searchPokes={this.searchPokes}  />
+						<PokePage {...props}  />
+					} />
+					<Route path='/viewPokemon/:id' component={(props)=>
+						<PokeDetails {...props} />
 					} />
 					<Route path='/' component={(props)=>
-						<PokePage {...props} searchPokes={this.searchPokes}  />
+						<PokePage {...props}  />
 					} />
 				</Switch>
 			</BrowserRouter>
